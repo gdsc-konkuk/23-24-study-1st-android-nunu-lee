a3d5498471c98fdda3927c4bb3e487e44fb34c3c
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-10-28 07:34:55 +0900

    :tada: INITIAL COMMIT

---
+++ README.md
@@ -0,0 +1,3 @@
+# GDSC Android Study
+
+Hi!
---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kotlinAndroid)
+}
+
+android {
+    namespace = "kr.ac.konkuk.gdsc.nunu"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "kr.ac.konkuk.gdsc.nunu"
+        minSdk = 28
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
